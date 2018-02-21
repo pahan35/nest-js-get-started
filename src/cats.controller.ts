ce281c9fa884ccff1029836019e4e90acbea0c07
Pavlo Zhukov <pahazhukov@gmail.com>
2018-02-22 00:50:56 +0200

    Controller 3

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+    @HttpCode(204)
     @Post()
     create() {
         // TODO: Add some logic here
@@ -10,5 +11,11 @@ export class CatsController {
     @Get()
     findAll(@Req() request) {
         return [];
+    }
+
+    @Get(':id')
+    findOne(@Param() params) {
+        console.log(params.id);
+        return {};
     }
 }
