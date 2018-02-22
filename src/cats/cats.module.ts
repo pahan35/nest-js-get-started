8c45ddcfe18ea5fb5ca70167d69672943481d353
Pavlo Zhukov <pahazhukov@gmail.com>
2018-02-22 04:07:40 +0200

    Module 2

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
+@Global()
 @Module({
   controllers: [CatsController],
   components: [CatsService],
+  exports: [CatsService]
 })
 
 export class CatsModule {
-  
+    constructor(private readonly catsService: CatsService) {}
 }
