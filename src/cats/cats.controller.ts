759b7d1c18b089488b6b12a0378716b9f6553b7f
Pavlo Zhukov <pahazhukov@gmail.com>
2018-02-25 20:53:48 +0200

    Pipe 3

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,9 @@
-import { Controller, Get, Param, Post, Res, Body, HttpCode, UsePipes } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
-import { ValidationPipe } from '../validation.pipe';
 
 @Controller('cats')
-@UsePipes(new ValidationPipe())
 export class CatsController {
     constructor(private readonly catsService: CatsService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { ValidationPipe } from './validation.pipe';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
 	await app.listen(3000);
+    app.useGlobalPipes(new ValidationPipe());
 }
 bootstrap();
