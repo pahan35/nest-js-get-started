a44c330e5b319ffc7a1698fdf37f70c0c95fc250
Pavlo Zhukov <pahazhukov@gmail.com>
2018-02-26 00:12:55 +0200

    Guard 1

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Param, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, UseGuards } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 import { ParseIntPipe } from '../parse-int.pipe';
+import { RolesGuard } from '../roles.guard';
 
 @Controller('cats')
+@UseGuards(RolesGuard)
 export class CatsController {
     constructor(private readonly catsService: CatsService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from './validation.pipe';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
-	await app.listen(3000);
     app.useGlobalPipes(new ValidationPipe());
+	await app.listen(3000);
 }
+
 bootstrap();
