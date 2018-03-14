f455bb19feb69a533e81e613d6dd9fc5d21c3a4e
Pavlo Zhukov <pahazhukov@gmail.com>
2018-03-14 03:29:11 +0200

    Return object after validation. Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { ValidationPipe } from './validation.pipe';
-import { RolesGuard } from './roles.guard';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
--- src/validation.pipe.ts
+++ src/validation.pipe.ts
@@ -14,7 +14,7 @@ export class ValidationPipe implements PipeTransform<any> {
         if (errors.length > 0) {
             throw new BadRequestException('Validation failed');
         }
-        return value;
+        return object;
     }
 
     private toValidate(metatype): boolean {
